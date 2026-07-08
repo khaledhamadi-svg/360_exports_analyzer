539718259b6a738136e6942342ae07272d4b166b
Khaled Hamadi <khaled.hamadi@yassir.com>
2026-07-08 09:25:20 +0100

    Added the rest of the datetime columns transformations

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ order_at = pd.to_datetime(admin_earnings['order_day'].astype(str) + " " + admin_
 admin_earnings.insert(order_day_idx, 'order_at', order_at)
 
 admin_earnings = admin_earnings.drop(columns=['order_day', 'order_time'])
-print(admin_earnings.head(10))
 
 # Merge *delivered_day* and *delivered_at* columns to create *delivered_at* column as timestamp type
 delivered_day_idx = admin_earnings.columns.get_loc('delivered_day')
@@ -33,7 +32,50 @@ admin_earnings.insert(delivered_day_idx, 'delivered_at_clean', delivered_at_clea
 
 admin_earnings = admin_earnings.drop(columns=['delivered_day', 'delivered_at'])
 admin_earnings.rename({'delivered_at_clean': 'delivered_at'})
-print(admin_earnings.head(10))
 
 
+# Format the columns into date time
 
+admin_earnings["accepted_at"] = pd.to_datetime(admin_earnings["accepted_at"])
+admin_earnings["cancelled_at"] = pd.to_datetime(admin_earnings["cancelled_at"])
+
+print(len(admin_earnings))
+
+# Remove irrelevant rows
+admin_earnings = admin_earnings[
+    ~admin_earnings["restaurant_name"].str.contains(
+        "Market", case=False, na=False
+    )
+]
+admin_earnings = admin_earnings[
+    ~admin_earnings["restaurant_name"].str.contains(
+        "City center", case=False, na=False
+    )
+]
+admin_earnings = admin_earnings[
+    ~admin_earnings["restaurant_name"].str.contains(
+        "uno", case=False, na=False
+    )
+]
+admin_earnings = admin_earnings[
+    ~admin_earnings["restaurant_name"].str.contains(
+        "test", case=False, na=False
+    )
+]
+admin_earnings = admin_earnings[
+    ~admin_earnings["restaurant_name"].str.contains(
+        "Forfait", case=False, na=False
+    )
+]
+admin_earnings = admin_earnings[
+    ~admin_earnings["cancellation_reason"].str.contains(
+        "rembour", case=False, na=False
+    )
+]
+admin_earnings = admin_earnings[
+    ~admin_earnings["cancellation_reason"].str.contains(
+        "test", case=False, na=False
+    )
+]
+
+print(len(admin_earnings))
